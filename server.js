fa8862c7e228a2b43e8d3fb3fde4747e2622fabf
Jc <jcjimenezz23@gmail.com>
2017-08-07 04:04:51 -0400

    set mongoose.promise and handlebars

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
  //requiring dependencies
- var cheerio = require("cheerio");
- var request = require("request");
+ var logger = require("morgan");
  var express = require("express");
  var mongoose = require("mongoose");
  var bodyParser = require("body-parser");
  var exphbs = require("express-handlebars");
 
-//express app
+ //Scraping tools
+ var cheerio = require("cheerio");
+ var request = require("request");
+
+//Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//initialize express
 var app = express();
-var PORT = process.env.PORT || 8080;
+ 
